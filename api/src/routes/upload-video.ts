705b393ad60f188bc64866ec3e1d0944acaed57b
Arthur Machado <arthur.machado@iclubs.tech>
2023-09-13 20:46:03 -0300

    remove wrong package

--- api/src/routes/upload-video.ts
+++ api/src/routes/upload-video.ts
@@ -1,8 +1,19 @@
 import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from '@fastify/multipart';
 import { prisma } from "../lib/prisma";
 
-export async function getAllPromptsRoute(app: FastifyInstance) {
-  app.post('/videos', async () => {
-    
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 25, // 25mb
+    }
+  })
+
+  app.post('/videos', async (request, reply) => {
+    const data = await request.file()
+
+    if(!data) {
+      return reply.status(400).send({ error: 'Missing file input' })
+    }
   })
 }
