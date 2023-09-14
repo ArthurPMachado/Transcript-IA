58dd1b156621769cae3846a94cd6b8b6b647e441
Arthur Machado <arthur.machado@iclubs.tech>
2023-09-13 22:07:08 -0300

    fix extension validation and update gitignore

--- api/src/routes/upload-video.ts
+++ api/src/routes/upload-video.ts
@@ -25,7 +25,9 @@ export async function uploadVideoRoute(app: FastifyInstance) {
 
     const extension = path.extname(data.filename)
 
-    if(extension !== 'mp3') {
+    console.log(extension)
+
+    if(extension !== '.mp3') {
       return reply.status(400).send({ error: 'Invalid input type, please upload a MP3.' })
     }
 
@@ -34,5 +36,7 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
 
     await pump(data.file, fs.createWriteStream(uploadDestination))
+
+    return reply.send()
   })
 }
